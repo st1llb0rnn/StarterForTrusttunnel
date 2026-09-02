9f0f67b002691cdaa2ea32746ec0a7a26d3fb4d9
st1llb0rn <artyommorozov340@gmail.com>
2026-09-02 17:04:33 +0300

    Init textual lib in project

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def start_procces():
-    print("test_procces")
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+class StarterForTrusttunnel (App):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
 
 if __name__ == "__main__":
-    start_procces()
+    app = StarterForTrusttunnel()
+    app.run()
